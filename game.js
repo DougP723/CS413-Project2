2ec69a8ac505a97accabd86e4c89bdfa1be52c56
DougP723 <dougpeterson23@gmail.com>
2016-05-23 11:21:39 -0700

    Added miner sprite to game
    also implemented keyboard controls

--- game.js
+++ game.js
@@ -1,25 +1,65 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
+var renderer = PIXI.autoDetectRenderer(800, 800, {backgroundColor: 0x3344ee});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-var texture = PIXI.Texture.fromImage("cs413.png");
+PIXI.loader
+	.add("minerWalk.json")
+	.load(ready);
 
-var sprite = new PIXI.Sprite(texture);
+function ready(){
+	var standing = new PIXI.Sprite(PIXI.Texture.fromFrame("miner8.png"));
+	standing.scale.x = 4;
+	standing.scale.y = 4;
+	standing.position.x = 50;
+	standing.position.y = 200;
+	stage.addChild(standing);
 
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
+	var frames = [];
+	for (var i=1; i<=8; i++) {
+		frames.push(PIXI.Texture.fromFrame('miner' + i + '.png'));
+	}
 
-sprite.position.x = 200;
-sprite.position.y = 200;
+	var walking = new PIXI.extras.MovieClip(frames);
+	walking.scale.x = 1;
+	walking.scale.y = 1;
+	walking.position.x = 200;
+	walking.position.y = 200;
+	walking.animationSpeed = 0.4;
+	walking.play();
+	stage.addChild(walking);
 
-stage.addChild(sprite);
+	//Keyboard controls
+	function keydownEventHandler(e) {
+
+		//if (e.keyCode == 87) { //W key
+			//walking.position.y -= 5;
+		//}
+
+		//if (e.keyCode == 83) { //S key
+			//walking.position.y +=5;
+		//}
+
+		if (e.keyCode == 65) { //A key
+			walking.position.x -= 5;
+			walking.scale.x = -1;	
+		}
+
+		if (e.keyCode == 68) { //D key
+			walking.position.x += 5;
+			walking.scale.x = 1;
+		}
+	}
+
+	document.addEventListener('keydown', keydownEventHandler);
+
+}
+
 
 function animate() {
 	requestAnimationFrame(animate);
-	sprite.rotation += 0.1;
 	renderer.render(stage);
 }
 animate();
